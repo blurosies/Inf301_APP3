2f40e43783c80b86be216e5d105c478036f28855
blurosies <nadermarieange@gmail.com>
2024-12-03 14:09:48 +0100

    initial commit

---
+++ listes.c
@@ -0,0 +1,32 @@
+#include "listes.h"
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/* fichier à compléter au besoin */
+
+void init_liste_vide(liste_t *L)
+{
+       printf ("<<<<< À faire: fonction init_liste_vide fichier " __FILE__ " >>>>>\n");
+    /* a completer */
+}
+
+/*
+ * Supprime toutes les cellules d'une liste L, sans faire de free sur
+ * L elle-même.
+ */
+void liberer_liste(liste_t *L)
+{
+       printf ("<<<<< À faire: fonction liberer_liste fichier " __FILE__ " >>>>>\n");
+    /* a completer */
+}
+
+
+int ajouter_tete(liste_t *L, string c)
+{   /* retourne 0 si OK, 1 sinon  */
+       printf ("<<<<< À faire: fonction ajouter_tete fichier " __FILE__ " >>>>>\n");
+    /* a completer */
+    return 1;
+}
+
